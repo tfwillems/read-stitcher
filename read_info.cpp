1f9fcf7c3c320171336005732311e7e0e7801a9c
Thomas Willems <willems@usmac02rd20rfvh6m.bos1.vrtx.com>
2016-11-08 23:18:53 -0500

    Added functions to trim N tails or low quality tails from reads

--- read_info.cpp
+++ read_info.cpp
@@ -1,2 +1,54 @@
 #include "read_info.h"
 
+void ReadInfo::trimNTails(){
+  int start = 0, end = sequence.size()-1;
+  while (start < sequence.size()){
+    if (sequence[start] != 'N')
+      break;
+    start++;
+  }
+
+  while (end >= 0){
+    if (sequence[end] != 'N')
+      break;
+    end--;
+  }
+
+  if (start <= end){
+    std::string new_seq  = sequence.substr(start, end-start+1);
+    std::string new_qual = quality.substr(start,  end-start+1);
+    sequence = new_seq;
+    quality  = new_qual;
+  }
+  else {
+    sequence = "";
+    quality  = "";
+  }
+}
+
+
+void ReadInfo::trimLowQualityEnds(char min_qual){
+  int start = 0, end = sequence.size()-1;
+  while (start < quality.size()){
+    if (quality[start] >= min_qual)
+      break;
+    start++;
+  }
+
+  while (end >= 0){
+    if (quality[end] >= min_qual)
+      break;
+    end--;
+  }
+
+  if (start <= end){
+    std::string new_seq  = sequence.substr(start, end-start+1);
+    std::string new_qual = quality.substr(start,  end-start+1);
+    sequence = new_seq;
+    quality  = new_qual;
+  }
+  else {
+    sequence = "";
+    quality  = "";
+  }
+}
--- read_info.h
+++ read_info.h
@@ -1,6 +1,8 @@
 #ifndef READ_INFO_H
 #define READ_INFO_H
 
+#include <assert.h>
+
 #include <string>
 #include <fstream>
 
@@ -12,14 +14,23 @@ private:
 
 public:
   ReadInfo(std::string identifier, std::string sequence, std::string quality){
+    assert(sequence.size() == quality.size());
     this->identifier = identifier;
-    this->sequence  = sequence;
-    this->quality   = quality;
+    this->sequence   = sequence;
+    this->quality    = quality;
   }	
 
   const std::string& get_identifier(){ return this->identifier;}
   const std::string& get_sequence()  { return this->sequence;  }
   const std::string& get_quality()   { return this->quality;   }
+
+  void trimNTails();
+
+  void trimLowQualityEnds(char min_qual);
+
+  bool empty(){
+    return sequence.size() == 0;
+  }
 };
 
 #endif
