8270128cdfb629d86c19eaad917b1be0f5c031bd
Thomas Willems <willems@usmac02rd20rfvh6m.bos1.vrtx.com>
2017-01-13 13:25:13 -0500

    Track the number of overlapping/mismatched bases and reporting them in the stitched read ID

--- read_stitcher.cpp
+++ read_stitcher.cpp
@@ -28,7 +28,7 @@ void ReadStitcher::printStitching(const std::string& s1, const std::string& s2, 
   std::cout << spacing << s2 << std::endl;
 }
 
-void ReadStitcher::kMismatch(const std::string& s1, const std::string& s2, int* max_match_idx, int* max_matches, int* best_frac_idx, double* best_frac){
+void ReadStitcher::kMismatch(const std::string& s1, const std::string& s2, int* best_frac_idx, double* best_frac, int& num_bp_overlap, int& num_mismatches){
   std::string exp = s1 + '#' + s2;
   char separator  = '#';
   SuffixTree tree(exp);
@@ -36,8 +36,10 @@ void ReadStitcher::kMismatch(const std::string& s1, const std::string& s2, int* 
   lca->processTree(tree);
   *best_frac      = 0;
   *best_frac_idx  = -1;
-  *max_matches    = -1;
-  *max_match_idx  = -1;
+  num_bp_overlap  = -1;
+  num_mismatches  = -1;
+  //*max_matches    = -1;
+  //*max_match_idx  = -1;
 
   for (int i = 0; i < ((int)s1.size()) - min_bp_overlap; i++){
     int sfx_offset_1 = 0;
@@ -67,23 +69,32 @@ void ReadStitcher::kMismatch(const std::string& s1, const std::string& s2, int* 
     if (i+sfx_offset_1 == s1.size() || (s2.size() >= min_bp_overlap && sfx_offset_2 == 1+s2.size())){
       double frac = 1.0*(sfx_offset_1-k)/sfx_offset_1;
 
-      // Check if stiching satisfies minimum fraction requirement
+      // Check if stitching satisfies minimum fraction requirement
       if (frac > min_frac_correct){
 	if (frac > *best_frac){
 	  *best_frac     = frac;
 	  *best_frac_idx = i;
+	  num_bp_overlap = sfx_offset_1;
+	  num_mismatches = k;
+
+	  // We've found a perfect match that meets all of the requirements. Because we're scanning from left to right,
+	  // this is also the maximal match, so we're certain this is the best result and can abort the search
+	  if (k == 0)
+	    return;
 	}
 
+	/*
 	if (sfx_offset_1-k > *max_matches){
 	  *max_matches   = sfx_offset_1-k;
 	  *max_match_idx = i;
 	}
+	*/
       }
     }
   }
 }
 
-ReadInfo ReadStitcher::merge_read_information(ReadInfo& r1, ReadInfo& r2, int stitch_index){
+ReadInfo ReadStitcher::merge_read_information(ReadInfo& r1, ReadInfo& r2, int stitch_index, int num_bp_overlap, int num_mismatches){
   std::string sequence = r1.get_sequence().substr(0, stitch_index);
   std::string quality  = r1.get_quality().substr(0, stitch_index);
 
@@ -105,6 +116,11 @@ ReadInfo ReadStitcher::merge_read_information(ReadInfo& r1, ReadInfo& r2, int st
       sequence += s2[i];
       quality  += q2[i];
     }
+
+    if (s1[i] != s2[i])
+      mismatch_base_quals_[std::min(q1[i], q2[i])]++;
+    else
+      match_base_quals_[std::min(q1[i], q2[i])]++;
   }
 
   // Trailing portion of stitched read
@@ -117,16 +133,14 @@ ReadInfo ReadStitcher::merge_read_information(ReadInfo& r1, ReadInfo& r2, int st
     quality  += q2.substr(i);
   }
 
-  return ReadInfo("STITCHED_" + r1.get_identifier(), sequence, quality, false);
+  return ReadInfo("STITCHED_" + std::to_string(num_bp_overlap) + "_" + std::to_string(num_mismatches) + "_" + r1.get_identifier() , sequence, quality, false);
 }
 
-int ReadStitcher::stitch_reads(const std::string& s1, const std::string& s2){
-  int max_match_idx;
-  int max_matches;
+int ReadStitcher::stitch_reads(const std::string& s1, const std::string& s2, int& num_bp_overlap, int& num_mismatches){
   int best_frac_idx;
   double best_frac;
 
-  kMismatch(s1, s2, &max_match_idx, &max_matches, &best_frac_idx, &best_frac);
+  kMismatch(s1, s2, &best_frac_idx, &best_frac, num_bp_overlap, num_mismatches);
   if (best_frac_idx != -1)
     return best_frac_idx;
   else
@@ -144,7 +158,7 @@ void ReadStitcher::stitch_fastq(std::string fastq_f1, std::string fastq_f2, std:
   FASTQWriter f1_writer(output_prefix + "_1.fq.gz");
   FASTQWriter f2_writer(output_prefix + "_2.fq.gz");
   FASTQWriter stitched(output_prefix  + "_stitched.fq.gz");
-  int32_t skip_count = 0, success_count = 0, fail_count = 0;
+  int32_t N_skip_count = 0, length_skip_count = 0, success_count = 0, fail_count = 0;
 
   while (true){
     if (f1_reader.is_empty())
@@ -167,41 +181,47 @@ void ReadStitcher::stitch_fastq(std::string fastq_f1, std::string fastq_f2, std:
     char min_qual = '5';
     f1_read.trimLowQualityEnds(min_qual);
     f2_read.trimLowQualityEnds(min_qual);
-    if (f1_read.empty() || f2_read.empty())
+    if (f1_read.empty() || f2_read.empty()){
+      fail_count++;
       continue;
+    }
 
     // Skip reads that exceed the max length, as they'll break the LCA computation
-    if (f1_read.get_sequence().size() > max_read_len || f2_read.get_sequence().size() > max_read_len)
+    if (f1_read.get_sequence().size() > max_read_len || f2_read.get_sequence().size() > max_read_len){
+      f1_writer.write_read(f1_read);
+      f2_writer.write_read(f2_read);
+      length_skip_count++;
       continue;
+    }
 
     // Attempt to stitch the reads together
-    int max_match_idx;
-    int max_matches;
     int best_frac_idx;
     double best_frac;
+    int num_bp_overlap, num_mismatches;
 
     // Skip reads with N's, as the suffix tree doesn't accommodate it
     if (f1_read.get_sequence().find("N") != std::string::npos ||
 	f2_read.get_sequence().find("N") != std::string::npos){
-      skip_count++;
+      N_skip_count++;
       continue;
     }
 
-    kMismatch(f1_read.get_sequence(), f2_read.get_sequence(), &max_match_idx, &max_matches, &best_frac_idx, &best_frac);
+    kMismatch(f1_read.get_sequence(), f2_read.get_sequence(), &best_frac_idx, &best_frac, num_bp_overlap, num_mismatches);
     if (best_frac_idx != -1){
       // Stitching met requirements
+      //std::cout << num_bp_overlap << " " << num_mismatches << std::endl;
       //printStitching(f1_read.get_sequence(), f2_read.get_sequence(), best_frac_idx);
-      ReadInfo stitched_read = merge_read_information(f1_read, f2_read, best_frac_idx);
+      ReadInfo stitched_read = merge_read_information(f1_read, f2_read, best_frac_idx, num_bp_overlap, num_mismatches);
       stitched.write_read(stitched_read);
       success_count++;
     }
     else {
       // Retry stitching, reversing which read we assume comes upstream
-      kMismatch(f2_read.get_sequence(), f1_read.get_sequence(), &max_match_idx, &max_matches, &best_frac_idx, &best_frac);
+      kMismatch(f2_read.get_sequence(), f1_read.get_sequence(), &best_frac_idx, &best_frac, num_bp_overlap, num_mismatches);
       if (best_frac_idx != -1){
 	// Stitching met requirements
 	//printStitching(f2_read.get_sequence(), f1_read.get_sequence(), best_frac_idx);
-	ReadInfo stitched_read = merge_read_information(f2_read, f1_read, best_frac_idx);
+	ReadInfo stitched_read = merge_read_information(f2_read, f1_read, best_frac_idx, num_bp_overlap, num_mismatches);
 	stitched.write_read(stitched_read);
 	success_count++;
       }
@@ -214,12 +234,33 @@ void ReadStitcher::stitch_fastq(std::string fastq_f1, std::string fastq_f2, std:
     }
   }
 
-  std::cerr << "Skipped " << skip_count << " reads with N bases" << std::endl;
-  std::cerr << "Stitching succeeded for " << success_count << " out of " << (success_count+fail_count) << " pairs of reads (" << (100.0*success_count/(success_count+fail_count)) << "%)" << std::endl;
+  if (length_skip_count != 0)
+    std::cerr << "Skipped " << length_skip_count << " reads whose length was greater than " << max_read_len << "\n"
+	      << "\t" << "If this is a significant fraction of your dataset, consider increasing --max-read-length" << std::endl;
+  if (N_skip_count != 0)
+    std::cerr << "Skipped " << N_skip_count << " reads with N bases" << std::endl;
+  std::cerr << "Stitching succeeded for " << success_count << " out of " << (success_count+fail_count) << " remaining pairs of reads (" << (100.0*success_count/(success_count+fail_count)) << "%)" << std::endl;
 
   f1_reader.close();
   f2_reader.close();
   f1_writer.close();
   f2_writer.close();
   stitched.close();
+}
+
+
+void ReadStitcher::print_base_qual_stats(std::ostream& out){
+  int64_t match_total = 0;
+  for (auto count_iter = match_base_quals_.begin(); count_iter != match_base_quals_.end(); count_iter++)
+      match_total += count_iter->second;
+  for (auto count_iter = match_base_quals_.begin(); count_iter != match_base_quals_.end(); count_iter++)
+    out << count_iter->first << "\t" << count_iter->second << "\t" << 100.0*count_iter->second/match_total << "\n";
+  out << "\n";
+
+  int64_t mismatch_total = 0;
+  for (auto count_iter = mismatch_base_quals_.begin(); count_iter != mismatch_base_quals_.end(); count_iter++)
+    mismatch_total += count_iter->second;
+  for (auto count_iter = mismatch_base_quals_.begin(); count_iter != mismatch_base_quals_.end(); count_iter++)
+    out << count_iter->first << "\t" << count_iter->second << "\t" << 100.0*count_iter->second/mismatch_total << "\n";
+  out << "\n";
 }
--- read_stitcher.h
+++ read_stitcher.h
@@ -1,6 +1,8 @@
 #ifndef READ_STITCHER_H
 #define READ_STITCHER_H
 
+#include <iostream>
+#include <map>
 #include <string>
 
 #include "read_info.h"
@@ -14,16 +16,20 @@ private:
   double min_frac_correct;
   LCA *  lca;
 
+  std::map<char, int64_t> match_base_quals_;
+  std::map<char, int64_t> mismatch_base_quals_;
+
   void printStitching(const std::string& s1, const std::string& s2, int index);
-  ReadInfo merge_read_information(ReadInfo& r1, ReadInfo& r2, int stitch_index);
+  ReadInfo merge_read_information(ReadInfo& r1, ReadInfo& r2, int stitch_index, int num_bp_overlap, int num_mismatches);
 
 public:
   ReadStitcher(int max_read_len, int max_k, int min_bp_overlap, double min_frac_correct);
   ~ReadStitcher();
 
-  int stitch_reads(const std::string& s1, const std::string& s2);
+  int stitch_reads(const std::string& s1, const std::string& s2, int& num_bp_overlap, int& num_mismatches);
   void stitch_fastq(std::string fastq_f1, std::string fastq_f2, std::string output_prefix);
-  void kMismatch(const std::string& s1, const std::string& s2, int* max_match_idx, int* max_matches, int* best_frac_idx, double* best_frac);
+  void kMismatch(const std::string& s1, const std::string& s2, int* best_frac_idx, double* best_frac, int& num_bp_overlap, int& num_mismatches);
+  void print_base_qual_stats(std::ostream& out);
 };
 
 #endif
